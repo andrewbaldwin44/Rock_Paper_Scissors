51915ac99143986107f798f7b7e5863cdc8eb562
Andrew Baldwin <andrew.baldwin44@protonmail.com>
2020-02-22 00:15:11 -0500

    Completed the Rock, Paper, Scissors Game. The Javascript code to calculate who wins the game is complete. The CSS styling has also been completed.

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
-function gamePlay(usrChoice) {
+/*Values for Computer and User Score*/
+let usrScore = 0;
+let compScore = 0;
+
+/*Function to randomly select rock, paper, or scissors*/
+function compPlay() {
   let choices = ['rock', 'paper', 'scissors'];
-  let compChoice = choices[Math.floor(Math.random() * choices.length)];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
 
-  console.log(`You picked ${usrChoice} and the computer picked ${compChoice}.`)
+/*Gameplay*/
+function gamePlay(usrChoice) {
+  let compChoice = compPlay();
 
-  if (usrChoice == compChoice) {console.log("Tie Game!")}
-  else if (usrChoice == 'rock' && compChoice == 'scissors' || usrChoice == 'paper' && compChoice == 'rock' || usrChoice == 'scissors' && compChoice == 'paper') {
-    console.log("You Win!");
+  const usrScoreBoard = document.getElementById("usrScore");
+  const compScoreBoard = document.getElementById("compScore");
+  const result = document.getElementById("gameResult");
+
+  if (usrScore < 5 && compScore < 5) {
+    document.getElementById("usrPicked").innerHTML = usrChoice;
+    document.getElementById("compPicked").innerHTML = compChoice;
+
+    if (usrChoice == compChoice) {
+      result.innerHTML = "Tie Game!"
+      result.style.background = "rgb(218, 20, 192)";
+    }
+
+    else if (usrChoice == 'rock' && compChoice == 'scissors'
+    || usrChoice == 'paper' && compChoice == 'rock'
+    || usrChoice == 'scissors' && compChoice == 'paper') {
+      result.innerHTML = "You Win!";
+      result.style.background = "rgb(77, 218, 20)";
+      usrScore +=1;
+      usrScoreBoard.innerHTML = usrScore;
+    }
+
+    else {
+      result.innerHTML = "You Lose!";
+      result.style.background = "rgb(240, 128, 37)";
+      compScore+=1;
+      compScoreBoard.innerHTML = compScore;
+    }
   }
   else {
-    console.log("You Lose!");
+      document.getElementById("gameResult").innerHTML = "Lets Start a New Game!"
+  }
+  console.log(usrScore, compScore)
+
+}
+
+/*End of game*/
+function endGame() {
+  const result = document.getElementById("gameResult");
+  if (document.getElementById("usrScore").innerHTML.slice(-1) == 5) {
+    result.innerHTML = "You win the game!!!!";
+  }
+  if (document.getElementById("compScore").innerHTML.slice(-1) == 5) {
+    result.innerHTML = "You lose the game!";
   }
 }
